40ff628ac86b2ad2ac7be7ba85f3b4bdc3753185
andrew cohn <andrewcohn0@gmail.com>
2019-07-16 11:53:44 -0400

    THE DOM INTRO

---
+++ 23-dom-intro/src/index.js
@@ -0,0 +1,18 @@
+console.log('%c Hello, World!', 'color: red')
+//declaration
+let myVar;
+// assignment
+myVar = 'some val'
+
+// declaration AND assignment
+const cVar = 'michael scott is bankrupt'
+const container = document.querySelector('#container')
+
+dankMemes.forEach(function(meme) {
+  const cVar = 'haha swag lol'
+  // create an image tag
+  const img = document.createElement('img')
+  img.src = meme
+  // append that tag to the page
+  container.appendChild(img)
+})
