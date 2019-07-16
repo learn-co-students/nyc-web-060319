be0cbf71ade8e5a26b49d433da40ce3728cce0d9
andrew cohn <andrewcohn0@gmail.com>
2019-07-16 14:35:11 -0400

    after events lecture

--- 24-events-intro/src/index.js
+++ 24-events-intro/src/index.js
@@ -1,1 +1,39 @@
 console.log('%c nice', 'color: green')
+
+window.addEventListener('DOMContentLoaded', function() {
+  console.log('document is', document)
+  // DOM node .addEventListener('event', handler)
+
+  const form = document.querySelector('#comment-form')
+  form.addEventListener('submit', (eventObj) => {
+    console.log(eventObj)
+    eventObj.preventDefault()
+    console.log(eventObj.target)
+    console.log('FORM WUZ SUBMITTED')
+  })
+
+  document.querySelector("#helicopter-parent").addEventListener('click', (event) => {
+    // console.log(event.target.tagName)
+    if (event.target.tagName === 'BUTTON') {
+      switch (event.target.dataset.name) {
+        case "log":
+          console.log("THE LOG BUTTON WAS CLICKED")
+          break
+        case "alert":
+          alert("DANGER OPS")
+          break
+        case "error":
+          console.error("SOMETHING IS ON FIRE U SHOULD PANIC")
+          break;
+        default:
+
+      }
+    }
+  })
+
+  document.body.addEventListener('click', function (event) {
+    // console.log(event.target)
+    // console.log('OMG THE USER CLICKED SOMETHING')
+  })
+
+})
